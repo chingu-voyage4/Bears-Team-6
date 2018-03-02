1703fb4d75936b4b87f6e37c58762525ff630ecf
Vladimir Logachev <vlogachev@allmax.team>
2018-03-02 23:11:37 +0700

    feature: websockets wrapped in provider that stares current connection instance via actions

---
+++ src/redux/websocketProvider/actions.js
@@ -0,0 +1,65 @@
+// @flow
+
+import type { Socket } from '../../types'
+
+export const Types = {
+  START_CHANNEL: 'START_CHANNEL',
+  STOP_CHANNEL: 'STOP_CHANNEL',
+  CHANNEL_ON: 'CHANNEL_ON',
+  CHANNEL_OFF: 'CHANNEL_OFF',
+  SERVER_ON: 'SERVER_ON',
+  SERVER_OFF: 'SERVER_OFF',
+}
+
+export type StartChannelAction = { type: 'START_CHANNEL' }
+
+const startChannel = (): StartChannelAction => ({
+  type: Types.START_CHANNEL,
+})
+
+export type StopChannelAction = { type: 'STOP_CHANNEL' }
+
+const stopChannel = (): StopChannelAction => ({
+  type: Types.STOP_CHANNEL,
+})
+
+export type ChannelOnAction = { type: 'CHANNEL_ON' }
+
+const channelOn = (): ChannelOnAction => ({
+  type: Types.CHANNEL_ON,
+})
+
+export type ChannelOffAction = { type: 'CHANNEL_OFF' }
+
+const channelOff = (): ChannelOffAction => ({
+  type: Types.CHANNEL_OFF,
+})
+
+export type ServerOnAction = { type: 'SERVER_ON' }
+
+const serverOn = (socket: Socket): ServerOnAction => ({
+  type: Types.SERVER_ON,
+  socket,
+})
+
+export type ServerOffAction = { type: 'SERVER_OFF' }
+
+const serverOff = (): ServerOffAction => ({
+  type: Types.SERVER_OFF,
+})
+
+export const Creators = {
+  startChannel,
+  stopChannel,
+  channelOn,
+  channelOff,
+  serverOn,
+  serverOff,
+}
+
+export type Action = StartChannelAction
+  | StopChannelAction
+  | ChannelOnAction
+  | ChannelOffAction
+  | ServerOnAction
+  | ServerOffAction
---
+++ src/redux/websocketProvider/reducer.js
@@ -0,0 +1,49 @@
+// @flow
+
+import { Types } from './actions'
+import type { Action } from './actions'
+
+export type State = {
+  +channelStatus: 'on' | 'off',
+  +serverStatus: 'unknown' | 'on' | 'off',
+}
+
+const initialState: State = {
+  channelStatus: 'off',
+  serverStatus: 'unknown',
+}
+
+const channelOn = (state) => ({
+  ...state,
+  channelStatus: 'on',
+})
+
+const channelOff = (state) => ({
+  ...state,
+  channelStatus: 'off',
+})
+
+const serverOn = (state) => ({
+  ...state,
+  serverStatus: 'on',
+})
+
+const serverOff = (state) => ({
+  ...state,
+  serverStatus: 'off',
+})
+
+export const websocketProvider = (state: State = initialState, action: Action): State => {
+  switch (action.type) {
+    case Types.CHANNEL_ON:
+      return channelOn(state)
+    case Types.CHANNEL_OFF:
+      return channelOff(state)
+    case Types.SERVER_ON:
+      return serverOn(state)
+    case Types.SERVER_OFF:
+      return serverOff(state)
+    default:
+      return state
+  }
+}
