a35242851f338ca3ffb1d7523c1d9d8f72b46aeb
Sacha <sachamorgese@gmail.com>
2018-03-03 10:56:51 +0900

    chore: wired up redux dev tools

--- src/index.js
+++ src/index.js
@@ -17,7 +17,15 @@ import { Timestamp } from './components/Timestamp'
 import { reducers, sagas } from './redux'
 
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(reducers, applyMiddleware(sagaMiddleware))
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(sagaMiddleware),
+)
+/* eslint-enable */
+
 sagaMiddleware.run(sagas)
 const rootElement = document.createElement('div')
 
