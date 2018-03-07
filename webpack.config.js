4439db8818752010de835333d97526b6cd042293
Sacha <sachamorgese@gmail.com>
2018-03-07 23:54:29 +0900

    chore: removed stylefmt, using stylelint --fix

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,6 @@ module.exports = {
           'style-loader',
           'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
         ],
-        use: ['style-loader', 'css-loader'],
       }
     ],
   },
