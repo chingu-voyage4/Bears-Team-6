e1721ac9923c0bff1ff32ebb6085dec2b6761200
Sacha <sachamorgese@gmail.com>
2018-03-18 11:03:51 +0900

    WIP chore: add logout route

--- src/components/TempHeader/index.js
+++ src/components/TempHeader/index.js
@@ -12,6 +12,7 @@ const Header = () => (
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/usersettings">User Settings</Link></li>
         <li><Link to="/timestamp">Timestamp</Link></li>
+        <li><Link to="/logout">Logout</Link></li>
       </ul>
     </nav>
   </header>
