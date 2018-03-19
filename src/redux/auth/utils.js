07e1d8827109cea4d75a5f5455328a0417a5d4c9
Vladimir Logachev <vlogachev@allmax.team>
2018-03-19 09:02:58 +0700

    improved validations

--- src/redux/auth/utils.js
+++ src/redux/auth/utils.js
@@ -1,7 +1,17 @@
 // @flow
 
-export const isValidEmail = (x: string) => typeof x === 'string' && x.length > 6
-export const isValidPassword = (x: string) => typeof x === 'string' && x.length > 6
+import R from 'ramda'
+
+export const isValidEmail: string => boolean = R.allPass([
+  R.is(String),
+  R.test(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i),
+])
+
+export const isValidPassword = R.allPass([
+  R.is(String),
+  R.pipe(R.length, R.lte(10)),
+])
+
 export const isValidName = (x: string) => typeof x === 'string' && x.length > 1
 
 export const isValidRegistrationData = (name: string, email: string, password: string) =>
