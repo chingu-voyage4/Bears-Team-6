6780e02fc26f71e0cd6c9c489eeba03339933e5f
Sacha Morgese <sacha_morgese@waku-2.com>
2018-03-19 12:16:46 +0900

    fix: remove unnecessary files

--- move-build.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process')
-const os = require('os')
-
-const print = (str) => console.log(`${str}\n`)
-const printInfo = (str) => print(`   ... ${str}`)
-const execAndPrint = (command) => print(execSync(command))
-
-execAndPrint(`move "build\\*.*" ".\\..\\static"`)
