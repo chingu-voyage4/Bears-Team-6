1a6b21a874e6bf09b632e899d124c6ceb2274ef0
Sacha <sachamorgese@gmail.com>
2018-03-24 10:18:35 +0900

    WIP chore: add logout route

---
+++ src/components/TempHeader/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './styles.scss'
+
+// Temp header created to navigate through the screens
+
+const Header = () => (
+  <header className={styles.root}>
+    <nav>
+      <ul>
+        <li><Link to="/">Registration</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/usersettings">User Settings</Link></li>
+        <li><Link to="/timestamp">Timestamp</Link></li>
+        <li><Link to="/logout">Logout</Link></li>
+      </ul>
+    </nav>
+  </header>
+)
+
+export default Header
