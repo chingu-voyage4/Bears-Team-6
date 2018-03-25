47d45ba46a4a5166061a01998aeeabc36a829bce
Vladimir Logachev <vlogachev@allmax.team>
2018-03-26 06:10:00 +0700

    refactor: proper naming

--- src/redux/userSettings/saga.js
+++ src/redux/userSettings/saga.js
@@ -6,46 +6,63 @@ import { takeLatest, call, select, put } from 'redux-saga/effects'
 import config from '../../../application.config'
 import type { Saga } from '../../types'
 import { ActionTypes, Creators } from '..'
-import { devLog } from '../../utils'
+import { quietLog } from '../../utils'
+import type { SET_GEOPOSITION_MANUAL } from './actions'
 
+/**
+ * Will read geoposition from backend.
+ * * If not set, will get current geoposition by IP address (universal approach),
+ * * but will not save it to backend until user performs an action:
+ * * (fix geoposition manually or use geoposition in some interests)
+ */
 export function* watchPosition(): Saga<void> {
   try {
     const { host } = config
     const token = yield select(R.path(['auth', 'token']))
     const authConfig = { headers: { Authorization: `Bearer ${token}` } }
-
-    const userSettings = yield call(axios.get, `${host}/api/users/profile`, authConfig)
-    const latitude = R.path(['data', 'geolocation', 'latitute'], userSettings)
-    const longitude = R.path(['data', 'geolocation', 'longtitude'], userSettings)
-    devLog('userSettings', { latitude, longitude })
+    const userProfile = yield call(axios.get, `${host}/api/users/profile`, authConfig)
+    const latitude = R.path(['data', 'geolocation', 'latitude'], userProfile)
+    const longitude = R.path(['data', 'geolocation', 'longitude'], userProfile)
     if (latitude && longitude) {
       /**
        * Geoposition is already set
        */
-      devLog('Pos is already set')
       yield put(Creators.setGeopositionManual(latitude, longitude))
     } else {
       /**
        * Geoposition has not been set yet
        */
-      devLog('Pos has not been set yet')
       const res = yield call(axios.get, 'http://freegeoip.net/json/')
       const { latitude, longitude } = res.data
-      devLog('free geo ip', { latitude, longitude })
-      const reqBody = {
-        geolocation: {
-          latitute: latitude,
-          longtitude: longitude,
-        },
-      }
-      yield call(axios.put, `${host}/api/users/profile`, reqBody, authConfig)
       yield put(Creators.setGeoposition(latitude, longitude))
     }
   } catch (e) {
-    devLog(e)
+    quietLog(e)
   }
 }
 
-// TODO: when pos changes, post request.
+/**
+ * Will look for manual changes in geoposition and save it to backend when user performs an action:
+ * * (fix geoposition manually or use geoposition in some interests)
+ */
+export function* saveGeopositionOnBackend({ latitude, longitude }: SET_GEOPOSITION_MANUAL): Saga<void> {
+  try {
+    const { host } = config
+    const token = yield select(R.path(['auth', 'token']))
+    const authConfig = { headers: { Authorization: `Bearer ${token}` } }
+    const reqBody = {
+      geolocation: {
+        latitude,
+        longitude,
+      },
+    }
+    yield call(axios.put, `${host}/api/users/profile`, reqBody, authConfig)
+  } catch (e) {
+    quietLog(e)
+  }
+}
 
-export const userSettings = [takeLatest(ActionTypes.WATCH_POSITION, watchPosition)]
+export const userSettings = [
+  takeLatest(ActionTypes.WATCH_POSITION, watchPosition),
+  takeLatest(ActionTypes.SET_GEOPOSITION_MANUAL, saveGeopositionOnBackend),
+]
